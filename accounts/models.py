3fea274bc9cb5693f1e90336d46077682e19292c
marvin evins <maevins@cps.edu>
2020-04-13 13:16:14 -0500

    working on models

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     CATEGORY =(
@@ -20,8 +25,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price  = models.FloatField(null=True)
     category= models.CharField(max_length=200, null=True, choices = CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank = True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Order(models.Model):
@@ -30,7 +40,10 @@ class Order(models.Model):
         ('Out for Delivery','Out for Delivery'),
         ('Delivered','Delivered'),
     )
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product =  models.ForeignKey(Product,null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices = STATUS)
+
+    def __str__(self):
+        return self.name
